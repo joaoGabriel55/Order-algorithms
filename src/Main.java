1e11616680cc1c2644141b3a58f1ee846dcb105e
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2019-05-10 23:02:32 -0300

    Done merge sort

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
-public class Main {
+import orderAlgorithms.MergeSort;
 
+public class Main {
+    /*Algorithms:
+    Merge Sort
+    Quick Sort
+    Heap Sort
+    Hybrid: Quick e Heap */
     public static void main(String args[]) {
-        System.out.println("Hi");
+
+        int[] array = {5, 9, 7, 1, 2, 3};
+        print("Original Array: ");
+        printArray(array);
+        MergeSort.mergeSort(array, 0, array.length - 1);
+        print("\nOrder Array: ");
+        printArray(array);
+
+    }
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++)
+            System.out.print(array[i] + " ");
+    }
+
+    private static void print(Object o) {
+        System.out.print(o);
     }
 }
---
+++ src/orderAlgorithms/MergeSort.java
@@ -0,0 +1,66 @@
+package orderAlgorithms;
+
+public class MergeSort {
+
+    /*
+     * @params
+     * p = start
+     * q = end
+     * r = get the end of sub-array
+     * */
+    public static void merge(int[] array, int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        int i, j, k;
+
+        //New Arrays
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (i = 0; i < n1; i++)
+            left[i] = array[p + i];
+
+        for (j = 0; j < n2; j++)
+            right[j] = array[q + j + 1];
+
+        i = 0;
+        j = 0;
+        k = p;
+
+        while (j < n2 && i < n1) {
+            if (left[i] <= right[j]) {
+                array[k] = left[i];
+                i++;
+            } else {
+                array[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            array[k] = left[i];
+            i++;
+            k++;
+        }
+
+    /* Copy the remaining elements of R[], if there
+       are any */
+        while (j < n2) {
+            array[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(int[] array, int p, int r) {
+        if (p < r) {
+            int q = (p + r) / 2;
+            mergeSort(array, p, q);
+            mergeSort(array, q + 1, r);
+            merge(array, p, q, r);
+        }
+    }
+
+}
